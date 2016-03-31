b599c2fc4dc89d2859b16b75d2145cb2bde8f661
Suszyński Krzysztof <krzysztof.suszynski@coi.gov.pl>
2016-03-31 16:36:12 +0200

    Inital code import

---
+++ src/main/java/pl/wavesoftware/jmh/junit/utilities/JavaAgentSkip.java
@@ -0,0 +1,88 @@
+package pl.wavesoftware.jmh.junit.utilities;
+
+import com.google.common.annotations.VisibleForTesting;
+import org.junit.Assume;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.util.List;
+
+/**
+ * A rule that can be used to automatically skip test if some javaagent is being used for ex.: debugger,
+ * profiler, code coverage or other. It can be useful if you are running performance critical tests like JMH.
+ * <p>
+ * Example:
+ * <pre>
+ * &#64;ClassRule
+ * public static JavaAgentSkip javaAgentSkip = JavaAgentSkip.ifPresent();
+ * </pre>
+ * @author <a href="mailto:krzysztof.suszynski@coi.gov.pl">Krzysztof Suszynski</a>
+ * @since 29.03.16
+ */
+public final class JavaAgentSkip implements TestRule {
+
+    public static final String JAVAAGENT = "-javaagent:";
+    public static final String DEFAULT_MESSAGE_FORMAT = "Skipping test due to JavaAgentSkip rule set to %s";
+    private static final RuntimeMXBean DEFAULT_RUNTIME_MXBEAN = ManagementFactory.getRuntimeMXBean();
+    private final boolean isActive;
+    private final String messageFormat;
+    private final RuntimeMXBean runtimeMxBean;
+
+    private JavaAgentSkip(boolean isActive) {
+        this(isActive, DEFAULT_MESSAGE_FORMAT, DEFAULT_RUNTIME_MXBEAN);
+    }
+
+    @VisibleForTesting
+    protected JavaAgentSkip(boolean isActive, String messageFormat, RuntimeMXBean runtimeMxBean) {
+        this.isActive = isActive;
+        this.messageFormat = messageFormat;
+        this.runtimeMxBean = runtimeMxBean;
+    }
+
+    public static JavaAgentSkip ifPresent() {
+        return new JavaAgentSkip(true);
+    }
+
+    public static JavaAgentSkip ifPresent(String messageFormat) {
+        return new JavaAgentSkip(true, messageFormat, DEFAULT_RUNTIME_MXBEAN);
+    }
+
+    public static JavaAgentSkip ifNotPresent() {
+        return new JavaAgentSkip(false);
+    }
+
+    public static JavaAgentSkip ifNotPresent(String messageFormat) {
+        return new JavaAgentSkip(false, messageFormat, DEFAULT_RUNTIME_MXBEAN);
+    }
+
+    @Override
+    public Statement apply(final Statement base, Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                String message = String.format(messageFormat, isActive);
+                Assume.assumeTrue(message, assumeValue());
+                base.evaluate();
+            }
+        };
+    }
+
+    private boolean assumeValue() {
+        return isAgentThere() != isActive;
+    }
+
+    private boolean isAgentThere() {
+        List<String> arguments = runtimeMxBean.getInputArguments();
+        boolean agent = false;
+        for (String argument : arguments) {
+            if (argument.startsWith(JAVAAGENT)) {
+                agent = true;
+                break;
+            }
+        }
+        return agent;
+    }
+}
---
+++ src/main/java/pl/wavesoftware/jmh/junit/utilities/JmhCleaner.java
@@ -0,0 +1,103 @@
+package pl.wavesoftware.jmh.junit.utilities;
+
+import com.google.common.annotations.VisibleForTesting;
+import org.junit.Test;
+import org.junit.rules.ExternalResource;
+import pl.wavesoftware.eid.utils.EidPreconditions;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.net.URISyntaxException;
+
+import static pl.wavesoftware.eid.utils.EidPreconditions.checkState;
+import static pl.wavesoftware.eid.utils.EidPreconditions.tryToExecute;
+
+/**
+ * This class handles cleaning JMH test annotation that are being produced by JMH library.
+ * <p>
+ * Use it as a standard JUnit test rule or class rule. Pass a test class in constructor.
+ * <p>
+ * Example:
+ * <pre>
+ * &#64;ClassRule
+ * public static JmhCleaner cleaner = new JmhCleaner(MyClassTest.class);
+ * </pre>
+ * @author <a href="mailto:krzysztof.suszynski@coi.gov.pl">Krzysztof Suszynski</a>
+ * @since 25.03.16
+ */
+public final class JmhCleaner extends ExternalResource {
+    private static final String GENERATED_TEST_SOURCES = "generated-test-sources";
+    private static final String TEST_ANNOTATIONS = "test-annotations";
+    private final Class<?> testClass;
+
+    public JmhCleaner(Class<?> testClass) {
+        this.testClass = validateTestClass(testClass);
+    }
+
+    private Class<?> validateTestClass(Class<?> testClass) {
+        boolean hasTests = false;
+        for (Method method : testClass.getDeclaredMethods()) {
+            Test annot = method.getAnnotation(Test.class);
+            if (Modifier.isPublic(method.getModifiers()) && !Modifier.isStatic(method.getModifiers()) && annot != null) {
+                hasTests = true;
+                break;
+            }
+        }
+        if (!hasTests) {
+            throw new IllegalArgumentException("You need to pass a test class to constructor of JmhCleaner!!");
+        }
+        return testClass;
+    }
+
+    @Override
+    protected void after() {
+        tryToExecute(new EidPreconditions.UnsafeProcedure() {
+            @Override
+            public void execute() throws IOException, URISyntaxException {
+                cleanup();
+            }
+        }, "20160331:151210");
+    }
+
+    @VisibleForTesting
+    protected File getGeneratedTestAnnotationsDir() throws URISyntaxException, IOException {
+        String location = testClass.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
+        File file = new File(location).getCanonicalFile().getParentFile();
+        return resolve(file, GENERATED_TEST_SOURCES, TEST_ANNOTATIONS);
+    }
+
+    private void cleanup() throws IOException, URISyntaxException {
+        File testAnnotationsPath = getGeneratedTestAnnotationsDir();
+        deleteRecursive(testAnnotationsPath);
+    }
+
+    private static void deleteRecursive(final File file) throws IOException {
+        //to end the recursive loop
+        if (!file.exists()) {
+            return;
+        }
+
+        //if directory, go inside and call recursively
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            files = files == null ? new File[0] : files;
+            for (File f : files) {
+                //call recursively
+                deleteRecursive(f);
+            }
+        }
+        //call delete to delete files and empty directory
+        boolean deleted = file.delete();
+        checkState(deleted, "20160331:151306", "Couldn't remove file: %s", file);
+    }
+
+    private File resolve(File parent, String... paths) {
+        StringBuilder sb = new StringBuilder(parent.getPath());
+        for (String path : paths) {
+            sb.append(File.separator).append(path);
+        }
+        return new File(sb.toString());
+    }
+}
